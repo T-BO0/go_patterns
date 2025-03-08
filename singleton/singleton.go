04320f27d086f0cf6bd19a8a967ebf59859e7b1b
T-BO0 <t.tabatadze13@gmail.com>
2025-03-08 11:16:36 +0400

    singleton no comments

--- singleton/singleton.go
+++ singleton/singleton.go
@@ -2,22 +2,18 @@ package singleton
 
 import "sync"
 
-// SingletonStruct is a struct that will be used as a singleton
 type SingletonStruct struct {
 	data string
 }
 
-var instance *SingletonStruct // Singleton instance of SingletonStruct
-var syncOnce sync.Once        // sync.Once instance to make sure that the singleton is created only once
+var instance *SingletonStruct
+var syncOnce sync.Once
 
-// GetInstance is a function that will return the singleton instance
 func GetInstance() *SingletonStruct {
-	// Using sync.Once to make sure that the singleton is created only once
 	syncOnce.Do(func() {
 		instance = &SingletonStruct{
 			data: "Singleton Data",
 		}
 	})
-	// Returning the singleton instance of SingletonStruct. This will be the same instance every time this function is called
 	return instance
 }
